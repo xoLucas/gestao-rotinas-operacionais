fe1545078d5ac76903490dc7825027106e85f259
Lucas <lucasdousseau@gmail.com>
2026-05-29 16:00:34 -0300

    CREATE urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/auth/", include("apps.accounts.urls")), 
+    path("api/sync/", include("apps.sync.urls")),
+    path("api/execution/", include("apps.execution.urls")),
+    path("api/evidence/", include("apps.evidence.urls")),
 ]
